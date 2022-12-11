7ed438e250d13e629c41d39fea3bb246c9c167a7
jdht <jdht@jdhts-MacBook-Air.local>
2022-12-10 22:58:05 -0600

    added model Hit

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
 import uuid
 
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -28,3 +29,20 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.email
+
+
+class Hit(models.Model):
+    hitman = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="hits", related_query_name='hit', on_delete=models.CASCADE)
+    description = models.TextField()
+    target_name = models.CharField(max_length=50)
+    HIT_STATUS_CHOICES = [
+        ('1', 'Assigned'),
+        ('2', 'Failed.'),
+        ('3', 'Completed.'),
+    ]
+    status = models.CharField(
+        max_length=5,
+        choices=HIT_STATUS_CHOICES,
+        default='OPEN',
+    )
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="hits_author", related_query_name='hit_author', on_delete=models.CASCADE)
